00f29f71669a28650712074f24b0bcd8a330baf9
Danila Shutov <danila-shutov@yandex-team.ru>
2021-12-21 01:29:43 +0300

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import { stick } from "stick"
+
+stick((props: { header: string, content?: string }) => {
+    return <div>
+        <h1>{props.header}</h1>
+        <p>{props.content} text</p>
+    </div>
+}, {
+    tagName: "x-app",
+    reflect: {
+        header: true,
+    }
+})
