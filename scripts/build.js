a7a4056fd417b1aa145f4aca5fbc1c47bdd603d3
Danila Shutov <dcbrwn2@gmail.com>
2021-12-28 23:26:54 +0300

    Fix build script

--- scripts/build.js
+++ scripts/build.js
@@ -1,12 +1,25 @@
+import { URL } from 'url'
+import fs from 'fs'
 import shell from 'shelljs'
 
+const relative = (path) => new URL(path, import.meta.url)
+
 {
+  const dist = relative('../dist')
+  const pack = relative('../package.json')
+
   const buildResult = shell.exec('tsc')
 
   if (buildResult.code !== 0) {
     process.exit(buildResult.code)
   }
 
-  shell.cp('./package.json', './dist/')
   shell.cp('./README.md', './dist/')
+
+  const parsedPack = JSON.parse(fs.readFileSync(pack, { encoding: 'utf-8' }))
+
+  parsedPack.files = fs.readdirSync(dist)
+  delete parsedPack.devDependencies
+
+  fs.writeFileSync(relative('../dist/package.json'), JSON.stringify(parsedPack, null, 2))
 }
