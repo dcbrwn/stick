b6774f446b4baa489df2d6d542d9b04a7f0151d6
Danila Shutov <dcbrwn2@gmail.com>
2022-01-10 21:36:44 +0300

    Refactor `repeat` directive

--- src/directives/repeat.ts
+++ src/directives/repeat.ts
@@ -16,41 +16,42 @@ const repeat = <ItemType> (
 ): RenderResult => {
   const container = createContainer()
   const items: Item<ItemType>[] = []
-  let visibleItems = 0
+  let visibleCount = 0
 
-  const appendItems = (itemsToAppend: number): void => {
-    const cachedItems = Math.min(itemsToAppend, items.length - visibleItems)
+  const createItems = (itemsToCreate: number): void => {
+    for (let i = 0; i < itemsToCreate; i += 1) {
+      withRenderingContext(() => {
+        const [observe, notify] = observable<ItemType>()
+        const element = renderer(broadcast(observe), items.length)
+        items.push({ element: ensureElement(element), notify, mount: getMount() })
+      })
+    }
+  }
+
+  const expand = (itemsToAppend: number): void => {
+    const cachedItems = Math.min(itemsToAppend, items.length - visibleCount)
     const itemsToCreate = itemsToAppend - cachedItems
 
-    if (itemsToCreate > 0) {
-      for (let i = 0; i < itemsToCreate; i += 1) {
-        withRenderingContext(() => {
-          const [observe, notify] = observable<ItemType>()
-          const element = renderer(broadcast(observe), items.length)
-          items.push({ element: ensureElement(element), notify, mount: getMount() })
-        })
-      }
-    }
+    createItems(itemsToCreate)
 
-    for (let i = visibleItems, len = visibleItems + itemsToAppend; i < len; i += 1) {
+    for (let i = visibleCount, len = visibleCount + itemsToAppend; i < len; i += 1) {
       const item = items[i]
       container.appendChild(item.element)
       if (item.mount) item.unmount = item.mount()
     }
 
-    visibleItems += itemsToAppend
+    visibleCount += itemsToAppend
   }
 
-  const removeItems = (itemsToRemove: number): void => {
-    visibleItems -= itemsToRemove
+  const shrink = (itemsToRemove: number): void => {
+    const oldVisibleCount = visibleCount
+    visibleCount -= itemsToRemove
 
-    for (let i = visibleItems, len = items.length; i < len; i += 1) {
-      const item = items[i]
-      if (item.unmount) {
-        item.unmount()
-        item.unmount = undefined
-        container.removeChild(item.element)
-      }
+    for (let i = oldVisibleCount; i > visibleCount; i -= 1) {
+      const item = items[i - 1]
+      item.unmount!()
+      item.unmount = undefined
+      container.removeChild(item.element)
     }
   }
 
@@ -60,30 +61,38 @@ const repeat = <ItemType> (
     }
   }
 
-  onMount(() => {
-    for (let i = 0; i < visibleItems; i += 1) {
-      const item = items[i]
+  const handleCollectionUpdates = (collection: ItemType[]) => {
+    const difference = collection.length - visibleCount
 
-      if (item.mount) item.unmount = item.mount()
+    if (difference > 0) {
+      expand(difference)
+    } else if (difference < 0) {
+      shrink(-difference)
     }
 
-    const unmount = observe((collection) => {
-      const difference = collection.length - visibleItems
+    notifyVisibleChildren(items, collection)
+  }
 
-      if (difference > 0) {
-        appendItems(difference)
-      } else if (difference < 0) {
-        removeItems(-difference)
-      }
+  const mountVisibleItems = () => {
+    for (let i = 0; i < visibleCount; i += 1) {
+      const item = items[i]
+      if (item.mount) item.unmount = item.mount()
+    }
+  }
 
-      notifyVisibleChildren(items, collection)
-    })
+  const unmountVisibleItems = () => {
+    for (let i = 0, len = visibleCount; i < len; i += 1) {
+      items[i].unmount!()
+    }
+  }
+
+  onMount(() => {
+    mountVisibleItems()
+    const forgetCollection = observe(handleCollectionUpdates)
 
     return () => {
-      for (let i = 0, len = visibleItems; i < len; i += 1) {
-        items[i].unmount?.()
-      }
-      unmount()
+      forgetCollection()
+      unmountVisibleItems()
     }
   })
 
